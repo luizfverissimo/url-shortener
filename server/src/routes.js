2639856ca8cb57f4c53540ea806f0adf05ed8d1f
luizfverissimo <62751844+luizfverissimo@users.noreply.github.com>
2020-09-30 21:50:42 -0300

    get request START

--- server/src/routes.js
+++ server/src/routes.js
@@ -5,6 +5,8 @@ const moment = require('moment')
 
 const routes = express.Router()
 
+const trx = await db.transaction()
+
 routes.post('/shortener', async (req, res) => {
   const { url_origin, date } = req.body
 
@@ -15,7 +17,6 @@ routes.post('/shortener', async (req, res) => {
     .add(1, 'days')
     .format('YYYY-MM-DD')
 
-  const trx = await db.transaction()
   try {
     await trx('urls').insert({
       url_origin,
@@ -35,10 +36,38 @@ routes.post('/shortener', async (req, res) => {
 routes.get('/:url_short', async (req, res) => {
   const { url_short } = await req.params
 
-  const urlShortCode = url_short.split('/', 3)
-  console.log(urlShortCode)
+  const urlMount = `http://localhost:3333/${url_short}`
 
-  return res.send(200)
+  try {
+    const [urlObj] = await db('urls')
+      .select('*')
+      .where('url_short', '=', urlMount)
+
+      console.log(urlObj)
+    if(!urlObj) {
+      return res.status(404).json({ error: 'Link não existe' })
+    }
+
+    const now = moment().format('YYYY-MM-DD')
+    const dayDifference = moment(urlObj.expire_date).diff(now, 'days')
+
+    if(dayDifference > 0) {
+      return res.redirect(301, `${urlObj.url_origin}`)
+    } else {
+      try {
+        await trx('urls').where('url_short', '=', urlMount).del()
+
+        trx.commit()
+
+        res.status(404).json({ error: 'Link não é mais válido' })
+      } catch (err) {
+        console.log(err)
+        res.status(400).json({ error: 'Erro no banco de dados' })
+      }
+    }
+  } catch (err) {
+    return res.status(400).json({ error: err.message })
+  }
 })
 
 module.exports = routes
